1e00fd5de8532259605c45ddb8220d4d7eeed88b
Krysmoke <cristianelcrack128@gmail.com>
2026-09-22 18:11:00 +0000

    Agrega prueba de useEffect en montaje

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,11 @@
+import { useEffect } from 'react';
 import LoginForm from '../components/organisms/LoginForm';
 
 const Login = () => {
+  useEffect(() => {
+    console.log('el componente se montó');
+  }, []);
+
   const handleLogin = async (credentials) => {
     console.log('Enviando credenciales:', credentials);
     // Aquí va la llamada real, ej: await authService.login(credentials)
